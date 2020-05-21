c5b3a4e901e52f74e497167b8977c34e47b00c4b
Swathi8512 <swathi.venkatesh@capillarytech.com>
2020-05-22 00:54:28 +0530

    Hello api using APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HellowApiView(APIView):
     """Test APIView"""
+
+    serializers_class = serializers.HelloSerializers
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -18,3 +23,30 @@ class HellowApiView(APIView):
             'message': 'Hello world',
             'an_apiview': an_apiview
         })
+
+    def post(self, request, format=None):
+        """Post request"""
+
+        serializers_result = self.serializers_class(data=request.data)
+
+        if serializers_result.is_valid():
+            name = serializers_result.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializers_result.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """PUT request"""
+
+        return Response({ 'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """PATCH request"""
+
+        return Response({ 'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """DELETE request"""
+
+        return Response({ 'method': 'DELETE' })
